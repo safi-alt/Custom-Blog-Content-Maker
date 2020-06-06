92db37c5db3fe86750f7baddc634ad6332194e85
safi <safiazim8@gmail.com>
2020-06-07 01:50:46 +0500

    Adding request/response interceptors using axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
+
+//use() helps in registering new interceptor
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    //edit request config
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    //edit response config
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
