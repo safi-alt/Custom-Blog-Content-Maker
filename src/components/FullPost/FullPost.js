79aab1644885b0cfdad490524c595bbab9621da4
safi <safiazim8@gmail.com>
2020-06-06 23:50:24 +0500

    Dealing with infinite loops in componenetDidUpdate

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -8,12 +8,17 @@ class FullPost extends Component {
   };
   componentDidUpdate() {
     if (this.props.id) {
-      axios
-        .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-        .then((response) => {
-          //   console.log(response);
-          this.setState({ loadedPost: response.data });
-        });
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+          .then((response) => {
+            //   console.log(response);
+            this.setState({ loadedPost: response.data });
+          });
+      }
     }
   }
   render() {
