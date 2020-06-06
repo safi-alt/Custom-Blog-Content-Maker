a46c4991d7fcd4e65682bec5070b6d9fe4bcd52d
safi <safiazim8@gmail.com>
2020-06-06 22:06:53 +0500

    Rendering Fetched data to the screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,20 +6,23 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
+      this.setState({ posts: response.data });
+      //   console.log(response);
     });
   }
   //then() helps to collect the data b/c the get method is aysnc and after it is completed then() is executed
   render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} />;
+    });
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
