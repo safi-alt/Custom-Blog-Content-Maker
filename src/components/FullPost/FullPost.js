79c422656bc3b0455393171c6ea637322c641ac2
safi <safiazim8@gmail.com>
2020-06-06 23:38:09 +0500

    Fetching Data on Update

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "./FullPost.css";
 
 class FullPost extends Component {
+  state = {
+    loadedPost: null,
+  };
+  componentDidUpdate() {
+    if (this.props.id) {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+        .then((response) => {
+          //   console.log(response);
+          this.setState({ loadedPost: response.data });
+        });
+    }
+  }
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+
     if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading...!</p>;
+    }
+
+    if (this.state.loadedPost) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
-          <p>Content</p>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
