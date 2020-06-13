9f905ccf61594b4e2c3559eebfaf580118771228
safi <safiazim8@gmail.com>
2020-06-13 18:02:13 +0500

    Restructured app to only dispaly Posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,53 +1,15 @@
 import React, { Component } from "react";
-//import axios from "axios";
 import axios from "../../axios";
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import Posts from "./Posts/Posts";
+
+//import axios from "axios";
+// import Post from "../../components/Post/Post";
+// import FullPost from "./FullPost/FullPost";
+// import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return { ...post, author: "Safi" };
-        });
-        this.setState({ posts: updatedPosts });
-        //   console.log(response);
-      })
-      .catch((error) => {
-        // console.log(error);
-        this.setState({ error: true });
-      });
-  }
-  //then() helps to collect the data b/c the get method is aysnc and after it is completed then() is executed
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong!!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
     return (
       <div>
         <header className="Blog">
@@ -62,13 +24,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <Posts />
       </div>
     );
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+import "./Posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: [],
+    // selectedPostId: null,
+    // error: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return { ...post, author: "Safi" };
+        });
+        this.setState({ posts: updatedPosts });
+        //   console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        // this.setState({ error: true });
+      });
+  }
+  //then() helps to collect the data b/c the get method is aysnc and after it is completed then() is executed
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
