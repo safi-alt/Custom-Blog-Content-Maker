82e1203940299c9be45ab53a60af26b4465d014f
safi <safiazim8@gmail.com>
2020-06-13 20:20:06 +0500

    Implementing routing basics using component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -25,8 +25,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/" render={() => <h1>Home2</h1>} />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
