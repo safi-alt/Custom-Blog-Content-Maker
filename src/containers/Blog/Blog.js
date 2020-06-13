9f6ce6e464f7c4eff6dad6260ed1040ddc6cbd96
safi <safiazim8@gmail.com>
2020-06-13 20:11:29 +0500

    Implementing routing basics

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "../../axios";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
+import { Route } from "react-router-dom";
 
 //import axios from "axios";
 // import Post from "../../components/Post/Post";
@@ -24,7 +25,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home2</h1>} />
       </div>
     );
   }
