449509f531d1029a5f9996a7adbf700900094919
safi <safiazim8@gmail.com>
2020-06-25 00:55:35 +0500

    Conditional Redirect on NewPost

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import axios from "axios";
 import "./NewPost.css";
 
@@ -7,6 +8,7 @@ class NewPost extends Component {
     title: "",
     content: "",
     author: "Safi",
+    submitted: false,
   };
 
   postDataHandler = () => {
@@ -17,12 +19,18 @@ class NewPost extends Component {
     };
     axios.post("/posts", data).then((response) => {
       console.log(response);
+      this.setState({ submitted: true });
     });
   };
 
   render() {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />;
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
